749dbbc54b4b2427cefd124adc9a677a94bdf3fd
M2MAexe <markov.maksim.mm2@gmail.com>
2023-04-25 00:19:24 +0300

    Added first half of commands

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,22 +9,6 @@ import movies.*
  * @param args an array of console arguments
  */
 fun main(args: Array<String>) {
-    var k = Movie(name = "Str", coordinates = Coordinates(5.1, 5f), oscarsCount = 1, length = 1, genre = MovieGenre.COMEDY, mpaaRating = MpaaRating.PG_13)
-    var k1 = Movie(name = "Str1", coordinates = Coordinates(5.1, 5f), oscarsCount = 1, length = 1, genre = MovieGenre.COMEDY, mpaaRating = MpaaRating.PG_13)
-
-    var person = Person(name = "MMM", hairColor = Color.WHITE)
-    var col = person.getHairColor()
-
-    val mg: MovieManager = MovieManager()
-
-    var g = arrayOf(String())
-
-    val commandManager = CommandManager()
-
-    commandManager.addCommand(SaveCommand(mg))
-    commandManager.addCommand(HelpCommand(commandManager))
-
-    commandManager.getCommands()["help"]?.execute()
 
 }
 
